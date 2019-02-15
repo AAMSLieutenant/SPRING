57655512c283e942fa1f897b63061e5f9eeca5aa
AAMSLieutenant <rst1991@ukr.net>
2019-02-15 13:44:43 +0200

    #3 working with types, indexes and values of a bean;

--- src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
+++ src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
@@ -1,6 +1,5 @@
 package ru.javabegin.training.spring.impls.robot;
 
-import ru.javabegin.training.spring.impls.toshiba.ToshibaHand;
 import ru.javabegin.training.spring.interfaces.Hand;
 import ru.javabegin.training.spring.interfaces.Head;
 import ru.javabegin.training.spring.interfaces.Leg;
@@ -11,6 +10,50 @@ public class ModelT1000 implements Robot {
     private Hand hand;
     private Leg leg;
     private Head head;
+
+    private String color;
+    private int year;
+    private boolean soundEnabled;
+
+    public ModelT1000() {
+    }
+
+    public ModelT1000(Hand hand, Leg leg, Head head) {
+        super();
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+    }
+
+    public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
+        super();
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    public ModelT1000(String color, int year, boolean soundEnabled) {
+        super();
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    public void action() {
+        head.calc();
+        hand.catchSomething();
+        leg.go();
+        System.out.println("color: " + color);
+        System.out.println("year: " + year);
+        System.out.println("can play sound: " + soundEnabled);
+    }
+
+    public void dance() {
+        System.out.println("T1000 is dancing!");
+    }
 
     public Hand getHand() {
         return hand;
@@ -36,27 +79,28 @@ public class ModelT1000 implements Robot {
         this.head = head;
     }
 
-    public ModelT1000() {
+    public String getColor() {
+        return color;
     }
 
-    public ModelT1000(Hand hand, Leg leg, Head head) {
-        super();
-        this.hand = hand;
-        this.leg = leg;
-        this.head = head;
+    public void setColor(String color) {
+        this.color = color;
     }
 
-
-    public void fire() {
-        head.calc();
-        hand.catchSomething();
-        leg.go();
+    public int getYear() {
+        return year;
     }
 
-
-    public void dance() {
-        System.out.println("T1000 is dancing!");
+    public void setYear(int year) {
+        this.year = year;
     }
 
+    public boolean isSoundEnabled() {
+        return soundEnabled;
+    }
+
+    public void setSoundEnabled(boolean soundEnabled) {
+        this.soundEnabled = soundEnabled;
+    }
 
 }
--- src/main/java/ru/javabegin/training/spring/start/RobotConstructor.java
+++ src/main/java/ru/javabegin/training/spring/start/RobotConstructor.java
@@ -12,7 +12,7 @@ public class RobotConstructor {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-		t1000.fire();
+		t1000.action();
 		((ConfigurableApplicationContext)context).close();// закрытие контекста вручную
 		
 	}
